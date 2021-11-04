f229b0f86912058f99fec6a11b6f2f701337c725
kurukuruk <richard.k.pro@pm.me>
2021-11-04 11:43:55 +0100

    feat: suppression de foo.rb

--- foo.rb
+++
@@ -1,8 +0,0 @@
-require 'net/http'
-require 'json'
-
-# response = JSON(Net::HTTP.get(URI('https://api.chucknorris.io/jokes/random')).body)&.transform_keys(&:to_sym)
-response = JSON(Net::HTTP.get(URI('https://api.chucknorris.io/jokes/random'))).transform_keys(&:to_sym)
-
-p response
-p response[:value]
