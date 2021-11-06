c4c6b035cc35d43ccfe2e3f9c7bf3bd465141a9b
kurukuruk <richard.k.pro@pm.me>
2021-11-06 16:37:38 +0100

    suppression commentaires

--- norris/controller.rb
+++ norris/controller.rb
@@ -1,10 +1,5 @@
 # frozen_string_literal: true
 
-##
-# Le controller de Chuck Norris.
-# Permet d'appeler l'api des blagues sur Chuck Norris.
-# A charger dans l'appication, la classe gère ses controllers
-#
 module Drawsheep
   module Norris
     class Controller < Kernel::Controller
