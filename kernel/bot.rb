4c4a448f4a8df62db245c0d5db308805b0f0bbfc
kurukuruk <richard.k.pro@pm.me>
2021-11-07 12:59:11 +0100

    fix: help vue

--- kernel/bot.rb
+++ kernel/bot.rb
@@ -16,8 +16,8 @@ module Drawsheep
       command 'help' do |client, data, match|
         client.say(channel: data.channel, text: Support::Help.generate_help(match[:expression]))
         super client, data, match
-        # rescue StandardError => e
-        #   client.say(channel: data.channel, text: "Je rencontre un problème: #{e.message}.")
+      rescue StandardError => e
+        client.say(channel: data.channel, text: "Je rencontre un problème: #{e.message}.")
       end
     end
   end
