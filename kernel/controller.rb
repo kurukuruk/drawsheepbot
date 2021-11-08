da203dd6aa382df6bd1e55d829403598025e3cee
kurukuruk <richard.k.pro@pm.me>
2021-11-08 13:45:32 +0100

    moins de commentaires repetitif

--- kernel/controller.rb
+++ kernel/controller.rb
@@ -6,30 +6,18 @@ require 'sinatra/json'
 
 module Drawsheep
   module Kernel
-    ##
-    # Notre controlleur de base
-    #
     class BaseController < Sinatra::Base
-      ##
-      # configuration generale
-      #
       configure do
         set :public_folder, 'public'
-        set :title, 'DrawsheepBot' # code.richard
+        set :title, 'DrawsheepBot'
         set :author, 'Les drawhseepers'
         set :created_at, '2021'
         set :description, 'Web app pour DrawsheepBot'
       end
 
-      ##
-      # configuration développement
-      #
       configure :development do
-        # exceptions
         disable :raise_errors
         disable :show_exceptions
-
-        # reload
         register Sinatra::Reloader
         after_reload do
           puts "#{self} reloaded"
